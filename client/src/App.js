1578a1eb5cb6cac1d88813bbf6f64a9ed29329a2
Aarush-Goyal <goyalaarush92@gmail.com>
2021-01-31 16:10:30 +0530

    updated routes

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
     <Router>
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/cart" component={Cart} />
+        <Route path="/cart" exact component={Cart} />
       </Switch>
     </Router>
   );
